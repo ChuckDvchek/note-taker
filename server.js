b7228fa2a3b3d7933bd0b7d25cfcf2e93a3f41f9
CarsdanDvorachek <cdvchek@gmail.com>
2021-10-21 19:41:10 -0400

    updated for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 6120;
+const PORT = process.env.PORT || 6120;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
